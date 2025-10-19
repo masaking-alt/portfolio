4e59e0ca43065b5455417235e3e6855e1b209965
masaking-alt <banbenjianggui@gmail.com>
2025-10-19 16:52:15 +0900

    作品詳細のコメント追加

--- src/works.js
+++ src/works.js
@@ -3,7 +3,7 @@ export const works = [
   {
     id: 1,
     title: "Gemini謹製 夏のひまつぶしコレクション",
-    description: "夏休みといえば？そう、暇。とにかく暇。その暇潰しの一助となるサイト。数多くのミニゲームを遊ぶことができます。",
+    description: "夏休みといえば？そう、暇。とにかく暇。その暇潰しの一助となるサイト。数多くのミニゲームを遊ぶことができます。楽しさを求めず、ただ時間をつぶしたい人向け。本当にしょうもないゲームをたくさん制作しました。",
     imageUrl: "/gemini_hima.jpg",
     externalUrl: "https://6675d79a.himatubusi.pages.dev/",
   },
